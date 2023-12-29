13e90cd7e60b6acf94bd2314912af3472c397be2
fynnkunz <fynn@kunz-privat.de>
2023-12-29 13:42:41 +0200

    upgrade_manager

--- Scenes/upgrade_screen.gd
+++ Scenes/upgrade_screen.gd
@@ -17,13 +17,13 @@ var upgrade_buff_value = {
 
 
 func _ready():
-	upgrade_0_modifier()
+	upgrade_0_manager()
 	var upgrade_1 = upgrades.pick_random()
 	var upgrade_2 = upgrades.pick_random()
 	
 	
 
-func upgrade_0_modifier():
+func upgrade_0_manager():
 	var upgrade_0 = upgrades.pick_random()
 	
 	var upgrade_texture = upgrade_texture.get(upgrade_0)
@@ -32,6 +32,7 @@ func upgrade_0_modifier():
 	var button = $"BoxContainer/Upgrade 0"
 	
 	button.texture_normal = load(upgrade_texture)
+	
 	
 func _on_upgrade_0_pressed():
 	pass # Replace with function body.
